ec46cdd7dd031429e7d8911ad1cb2c272d40f359
Yevheniia Ukolova <ukolova_e@ukr.net>
2021-12-18 20:46:10 +0200

    fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,14 +6,14 @@ import { galleryItems } from './gallery-items';
 const galleryRef = document.querySelector('.gallery');
 
 const galleryMarkup = galleryItems
-.map(({preview, original, description}) => `<li class="gallery__item" >
+.map(({preview, original, description}) => `<div class="gallery__item" >
 <a class="gallery__link" href="${original}">
 <img
 class="gallery__image"
 src="${preview}"
 alt="${description}"
 />
-</a></li>`).join('');
+</a></div>`).join('');
 
 galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
 
