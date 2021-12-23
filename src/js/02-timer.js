b6b4da45614121b65bade97b7382a212ca452692
Yevheniia Ukolova <ukolova_e@ukr.net>
2021-12-23 23:58:04 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,9 +2,9 @@ import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
 const inputEl = document.querySelector('#datetime-picker');
 const buttonEl = document.querySelector('button[data-start]');
+const timerDiv = document.querySelector('.timer');
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
@@ -48,7 +48,7 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
         if(selectedDates[0] < Date.now()) {
-            Notify.warning('Please choose a date in the future');
+            Notify.failure('Please choose a date in the future');
             userDate = new Date();
         } else { 
             buttonEl.disabled = false;
@@ -64,27 +64,32 @@ class Timer  {
         this.timerId = null;
         buttonEl.disabled = true;
     }
-    timerStartOnButtonClick() {
+    timerStart() {
         if (this.isActive) {
             return;
         }
     this.isActive = true;
-    this.timerId = setInterval(()=>{
+    this.timerId = setInterval(()=> {
         const currentTime = Date.now();
-        const deltaTime = convertMs(userDate - currentTime);
-            //  console.log(deltaTime);
-            secondsEl.textContent = deltaTime.seconds;
-            minutesEl.textContent = deltaTime.minutes;
-            hoursEl.textContent = deltaTime.hours;
-            daysEl.textContent = deltaTime.days;
+        const deltaTime = userDate - currentTime;
+        const components = convertMs(deltaTime);
+
+                secondsEl.textContent = components.seconds;
+                minutesEl.textContent = components.minutes;
+                hoursEl.textContent = components.hours;
+                daysEl.textContent = components.days;
+                if (deltaTime <= 0) {
+                    this.timerStop();
+                    timerDiv.innerHTML = "Time is over!";
+                } 
     }, 1000)
 
     }
-    stopTimer(){
+    timerStop() {
         clearInterval(this.timerId);
     }
 }
 
     const timer = new Timer();
     flatpickr(inputEl, options);
-    buttonEl.addEventListener('click', () => timer.timerStartOnButtonClick());
+    buttonEl.addEventListener('click', () => timer.timerStart());
